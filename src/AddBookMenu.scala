3a9b95671dc62691a7b7eb4f99305b3faabb1312
Jagath <jagathmax2@gmail.com>
2019-03-11 12:51:09 +0530

    Payment Menu added

--- src/AddBookMenu.scala
+++ src/AddBookMenu.scala
@@ -15,3 +15,7 @@ class AddBookMenu {
     println("Book added successfully")
   }
 }
+
+
+
+
--- src/MainMenu.scala
+++ src/MainMenu.scala
@@ -5,11 +5,11 @@ class MainMenu {
     try {
       val bookList: ArrayList[Book] = new util.ArrayList[Book]()
       val book1: Book = new Book()
-      book1.name = "Jagath"
+      book1.name = "The Hobbit"
       book1.bookID = 1;
       bookList.add(book1)
       val book2: Book = new Book()
-      book2.name = "Kamal"
+      book2.name = "Lord of the Rings"
       book2.bookID = 2;
       bookList.add(book2)
       var exit: Boolean = false
@@ -20,17 +20,21 @@ class MainMenu {
           println("01. List down all the books.")
           println("02. View detailed information about a book.")
           println("03. Add a new book.")
-          println("04. Exit")
+          println("04. Purchase a book.")
+          println("05. Exit")
           println()
           println("Enter your choice and hit Enter")
           val input: Int = scala.io.StdIn.readInt()
           input match {
             case 1 => val listMenu: ListMenu = new ListMenu()
               listMenu.show(bookList)
-            case 2 => println("sad")
+            case 2 => val detailsMenu: BookDetailsMenu = new BookDetailsMenu()
+              detailsMenu.show(bookList)
             case 3 => val addBookMenu: AddBookMenu = new AddBookMenu()
               addBookMenu.show(bookList)
-            case 4 => exit = true
+            case 4 => val purchaseMenu: PurchaseMenu = new PurchaseMenu()
+              purchaseMenu.show(bookList)
+            case 5 => exit = true
           }
         }
         catch {
